40006bfb5bb8db0a8eecb0d60bd97e8374bc45f0
furkangumgol4-hub <furkangumgol4@gmail.com>
2025-11-20 16:37:24 +0530

    Update det.py

--- det.py
+++ det.py
@@ -1,16 +1,16 @@
 import sys
-if len(sys.argv) ==3:
-  script_name = sys.argv[0]
-  name = sys.argv[1]
-  rollno = sys.argv[2]
-  print("user provided inp values:")
+
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("user provided inp values:")
 else:
-  script_name=sys.argv[0]
-  name="gustavo"
-  rollno="99"
-  print("no inp given - using default values:")
-  
-print("Script Name:",script_name)
-print("student name:",name)
-print(" Roll No. ",rollno)
-  
+    script_name = sys.argv[0]
+    name = "gustavo"
+    rollno = "99"
+    print("no inp given - using default values:")
+
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll No.:", rollno)
